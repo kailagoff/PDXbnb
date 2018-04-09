2d0a34f20025b8000d19e14967f7e1d7951a8ef7
Kaila <kaila.goff@gmail.com>
2018-04-09 16:04:01 -0700

    styling of navbar and home components with new md bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,44 +1,42 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  constructor() {
+  constructor() {}
 
-  var currentImageIndex = -1,
-          maxImageIndex = 0,
-          images = [],
-          changeInterval = 3500;
+  ngOnInit() {
+    var currentImageIndex = -1,
+    maxImageIndex = 0,
+    images,
+    changeInterval = 3500;
 
-      // Prepares relevant variables to cycle throguh images
-      var setUp = function() {
-          images = document.images;
-          maxImageIndex = images.length;
-          currentImageIndex = 0;
-      };
+    // Prepares relevant variables to cycle throguh images
+    var setUp = function() {
+      images = document.images;
+      maxImageIndex = images.length;
+      currentImageIndex = 0;
+    };
 
-      // Changes the banner currently being displayed
-      var changeBanner = function() {
-          var i;
+        // Changes the banner currently being displayed
+    var changeBanner = function() {
+      var i;
 
-          currentImageIndex = (currentImageIndex >= maxImageIndex - 1) ? 0 : currentImageIndex += 1;
+      currentImageIndex = (currentImageIndex >= maxImageIndex - 1) ? 0 : currentImageIndex += 1;
 
-          for (i = 0; i < maxImageIndex; i += 1) {
-              images[i].hidden = (i !== currentImageIndex);
-          }
-      };
+      for (i = 0; i < maxImageIndex; i += 1) {
+          images[i].hidden = (i !== currentImageIndex);
+      }
+    };
 
-      // A function which is triggered following the `load` event
-      window.onload = function() {
-          setUp();
+    setUp();
 
-          images[currentImageIndex].hidden = false; // show the first banner image;
+    images[currentImageIndex].hidden = false; // show the first banner image;
 
-          setInterval(changeBanner, changeInterval); // following a delay, keep changing the banner image by the specified interval
-      };
-    }
+    setInterval(changeBanner, changeInterval); // following a delay, keep changing the banner image by the specified interval
   }
+}
