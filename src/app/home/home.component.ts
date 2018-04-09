64e998974efab6174486ea8f29880b3a595f4e74
Guest User <Guest@Epicodus-6E.local>
2018-04-09 12:02:27 -0700

    add picture banner that intervals through images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
+  var currentImageIndex = -1,
+          maxImageIndex = 0,
+          images = [],
+          changeInterval = 2500;
+
+      // Prepares relevant variables to cycle throguh images
+      var setUp = function() {
+          images = document.images;
+          maxImageIndex = images.length;
+          currentImageIndex = 0;
+      };
+
+      // Changes the banner currently being displayed
+      var changeBanner = function() {
+          var i;
+
+          currentImageIndex = (currentImageIndex >= maxImageIndex - 1) ? 0 : currentImageIndex += 1;
+
+          for (i = 0; i < maxImageIndex; i += 1) {
+              images[i].hidden = (i !== currentImageIndex);
+          }
+      };
+
+      // A function which is triggered following the `load` event
+      window.onload = function() {
+          setUp();
+
+          images[currentImageIndex].hidden = false; // show the first banner image;
+
+          setInterval(changeBanner, changeInterval); // following a delay, keep changing the banner image by the specified interval
+      };
+    }
   }
-
-}
